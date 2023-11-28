de5329cb2d6eb91c2c8460653acc644577100c5e
GadourGuy <ogadours1@gmail.com>
2023-11-28 07:42:48 -0800

    uploding task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z') + 1):
-        if chr(i) == c and c.isalpha:
+        if chr(i) == c and c.isalpha():
             return True
     return False
