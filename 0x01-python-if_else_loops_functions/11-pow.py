815bcf81eca7654258033a0afe71f419a2f04f9d
GadourGuy <ogadours1@gmail.com>
2023-11-28 08:42:36 -0800

    uploding task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     c = 1
+    d = b
+    if b < 0:
+        b *= -1
     for i in range(b):
         c *= a
-    if b < 0:
-        return 1 / c
+    if d < 0:
+        return float(1 / c)
     return c
