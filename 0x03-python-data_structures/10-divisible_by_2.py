6786b5bfbd3d3e6be2ce5c5a0cb9bdda3813d55b
GadourGuy <ogadours1@gmail.com>
2023-12-02 07:04:37 -0800

    uploding task 13

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     new = [0 for i in range(len(my_list))]
     for i in range(len(my_list)):
-            if my_list[i] % 2 == 0:
-                new[i] = True
-            else:
-                new[i] = False
+        if my_list[i] % 2 == 0:
+            new[i] = True
+        else:
+            new[i] = False
     return new
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a[:2] + (0, 0)
     b = tuple_b[:2] + (0, 0)
-    sum  = (a[0] + b[0], a[1] + b[1])
+    sum = (a[0] + b[0], a[1] + b[1])
     return sum
