e957a3f69f3332d6315be90d5afa694f4208e347
GadourGuy <ogadours1@gmail.com>
2023-12-04 02:15:02 -0800

    uploding task 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,25 +1,18 @@
 #include <Python.h>
 
-/**
- * print_python_list_info - prints Python lists.
- * @p: object to be printed
- *
- * Return: void.
- */
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t size, alloc, i;
+    PyObject *item;
 
-void print_python_list_info(PyObject *p)
-{
-	int alloc, size, j;
-	PyObject *obj;
+    size = PyList_Size(p);
+    alloc = ((PyListObject *)p)->allocated;
 
-	size = Py_SIZE(p);
-	alloc = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc);
-	for (j = 0; j < size; j++)
-	{
-		printf("Element %d: ", j);
-		obj = PyList_GetItem(p, i);
-		printf("%s\n", Py_TYPE(obj)->tp_name);
-	}
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", alloc);
+
+    for (i = 0; i < size; ++i) {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
 }
+
