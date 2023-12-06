883c3855e1a5134c99b2678e572af99e42b609b5
GadourGuy <ogadours1@gmail.com>
2023-12-06 05:55:54 -0800

    uploding task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if not a_dictionary:
-        return None
-    keys = list(a_dictionary.keys())
-    new = a_dictionary.copy()
-    for i in keys:
-        new[i] *= 2
-    return new
+    return {a: b * 2 for a, b in a_dictionary.items()}
