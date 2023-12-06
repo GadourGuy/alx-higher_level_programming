535d0c65407f6751b7a8d1a097c3cd8e771a755b
GadourGuy <ogadours1@gmail.com>
2023-12-06 09:19:18 -0800

    uploding task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10,
+             'L': 50, 'C': 100, 'D': 500,
+             'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        if roman_string[i] in roman.keys():
+            if (i + 1) < len(roman_string):
+                if roman.get(roman_string[i]) < roman.get(roman_string[i+1]):
+                    big = roman.get(roman_string[i+1])
+                    result += (big - roman.get(roman_string[i]))
+                    i += 1
+                else:
+                    result += roman.get(roman_string[i])
+            else:
+                result += roman.get(roman_string[i])
+    return result
