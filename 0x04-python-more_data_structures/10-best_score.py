bd1d3315bb8c860573170a68e3f422acd4d0d40c
GadourGuy <ogadours1@gmail.com>
2023-12-06 06:14:42 -0800

    uploding task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    maxvalue = 0
+    maxkey = None
+    for key, value in a_dictionary.items():
+        if value > maxvalue:
+            maxvalue = value
+            maxkey = key
+    return maxkey
