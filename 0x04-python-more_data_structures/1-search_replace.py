ac31e4e67abc0f2da60015abe12076796ef94ca5
GadourGuy <ogadours1@gmail.com>
2023-12-06 03:45:43 -0800

    uploding task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
         return None
     my_list1 = my_list.copy()
     for i in range(len(my_list1)):
-        if str(my_list1[i]) == str(search):
+        if my_list1[i] == search:
             my_list1[i] = replace
     return my_list1
