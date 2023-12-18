304c7c3635a9e722dddb048e7cbca7708efb412a
GadourGuy <ogadours1@gmail.com>
2023-12-18 08:03:49 -0800

    uploding task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-        except IndexError:
+        except (IndexError, ValueError):
             break
         i += 1
     print()
