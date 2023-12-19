a3f006f05f022d249a2db82b947310edb801a51a
GadourGuy <ogadours1@gmail.com>
2023-12-19 13:07:05 -0800

    uploding task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+"""Square class that has size"""
 
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if int(value) < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """# returns square area"""
+        return self.__size ** 2
