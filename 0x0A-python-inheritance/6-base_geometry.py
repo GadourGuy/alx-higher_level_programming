ed2432da9f48a51cdfdc863841d6d97d67e0b1ef
GadourGuy <ogadours1@gmail.com>
2024-01-08 07:39:27 -0800

    uploading task 7

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Module for basic geometry"""
+
+
+class BaseGeometry:
+    """calculates basic geometry"""
+
+    def area(self):
+        """raises an error"""
+        raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Module for basic geometry"""
+
+
+class BaseGeometry:
+    """calculates basic geometry"""
+
+    def area(self):
+        """raises an error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """raises errors for values"""
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
