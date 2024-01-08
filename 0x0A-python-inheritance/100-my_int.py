afd9a4196e44de94b7ea9a88e23d64ad2df44701
GadourGuy <ogadours1@gmail.com>
2024-01-08 12:53:00 -0800

    uploading task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Module for comparing"""
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """Class MyInt that inhertied int"""
+    """Invert int operators == and !=."""
 
     def __eq__(self, value):
-        """Equale compare"""
-            return self.real != value
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
     def __ne__(self, value):
-        """Not Equale compare"""
-            return self.real == value
+        """Override != operator with == behavior."""
+        return self.real == value
