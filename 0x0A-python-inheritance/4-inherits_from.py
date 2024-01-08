564e6fd05da95d158a3c775b631a6452b5eed16b
GadourGuy <ogadours1@gmail.com>
2024-01-08 07:19:06 -0800

    uploading task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Checks if class is sub class or a class from a super class"""
+
+
+def inherits_from(obj, a_class):
+    """Checks for objects"""
+
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Module for empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
