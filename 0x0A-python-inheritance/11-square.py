2da90ae0a55b0826c4fd7fcb2bd28166559ee79c
GadourGuy <ogadours1@gmail.com>
2024-01-08 12:09:07 -0800

    uploading task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""importing rectangler file"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """representaion of a square object using Rectangle"""
+
+    def __init__(self, size):
+        """initiliaze a new square
+
+        Args:
+            size (int): size of a square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """"calcs area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string representaion of a square class"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
