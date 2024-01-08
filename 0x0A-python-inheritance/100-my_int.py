6acab39c99281a38902521eeb10295503911a7cc
GadourGuy <ogadours1@gmail.com>
2024-01-08 12:52:20 -0800

    uploading task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,10 +5,10 @@
 class MyInt(int):
     """Class MyInt that inhertied int"""
 
-    def __eq__(self, other):
+    def __eq__(self, value):
         """Equale compare"""
-            return self.real != other
+            return self.real != value
 
-    def __ne__(self, other):
+    def __ne__(self, value):
         """Not Equale compare"""
-            return self.real == other
+            return self.real == value
