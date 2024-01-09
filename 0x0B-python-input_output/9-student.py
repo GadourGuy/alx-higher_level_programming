6a64b2b8582763a3bb89c534f3a206511d7f8339
GadourGuy <ogadours1@gmail.com>
2024-01-09 13:56:36 -0800

    uploading task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Module for creating student class”"""
+import json
+import sys
+
+
+class Student:
+    """Student class with first and last names"""
+
+    def __init__(self, first_name, last_name, age):
+        """ATrributes: firstname, lastname, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
