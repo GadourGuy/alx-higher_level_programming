51e35d42a9610db753eb90781033f6eb1b4b7087
GadourGuy <ogadours1@gmail.com>
2024-01-09 13:30:38 -0800

    uploading task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Module for creating an Object from a “JSON file”"""
+import json
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+json_list = sys.argv[1:]
+try:
+    old = load_from_json_file("add_item.json")
+    save_to_json_file(old + json_list, "add_item.json")
+except FileNotFoundError:
+    save_to_json_file(json_list, "add_item.json")
