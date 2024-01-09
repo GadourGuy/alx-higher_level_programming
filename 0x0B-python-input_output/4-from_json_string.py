c0716e0987d7da7fda649d666d655fb7f5f3fb1c
GadourGuy <ogadours1@gmail.com>
2024-01-09 12:20:02 -0800

    uploading task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""module for the JSON representation of an object """
+import json
+
+
+def from_json_string(my_str):
+    """returns the JSON representation of an object"""
+    return json.loads(my_str)
