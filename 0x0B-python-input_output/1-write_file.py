5e258f539746698e82bdf3fbff445303ede5604c
GadourGuy <ogadours1@gmail.com>
2024-01-09 08:20:59 -0800

    uploading task 0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""module for writing in a file"""
+
+
+def write_file(filename="", text=""):
+    """For writing a File"""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+    return len(text)
