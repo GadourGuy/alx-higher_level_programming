7cee8cea549cd628d4cd809aa7d335738817328b
GadourGuy <ogadours1@gmail.com>
2024-01-09 08:25:07 -0800

    uploading task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""module for writing in a file"""
+
+
+def append_write(filename="", text=""):
+    """For writing a File"""
+    count = 0
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
