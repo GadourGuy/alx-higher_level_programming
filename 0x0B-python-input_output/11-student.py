b5a76be2128c36744ef0aa77ceaca488c91a6ad8
GadourGuy <ogadours1@gmail.com>
2024-01-09 14:14:19 -0800

    uploading task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Module for creating student class”"""
+
+
+class Student:
+    """Student class with first and last names"""
+
+    def __init__(self, first_name, last_name, age):
+        """ATrributes: firstname, lastname, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary represtaion of a class"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """loads attributes"""
+        for k, v in json.items():
+            setattr(self, k, v)
