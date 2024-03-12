279325bba09f212912f16907f456a1cd8340e4ad
Your Name <you@example.com>
2024-03-12 17:59:30 +0800

    adding task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        if (c === undefined) { row += 'X'; } else { row += c; }
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
