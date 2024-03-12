e91559a0ac7921f6d5377863b541b6fd87cb0ae0
Your Name <you@example.com>
2024-03-13 00:00:03 +0800

    adding task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  let tmp;
+  for (let i = 0; i < Math.floor(list.length / 2); i++) {
+    tmp = list[i];
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = tmp;
+  }
+  return (list);
+};
