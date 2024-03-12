f9e72e3940a722304c8c26f5b8b4972908700b13
Your Name <you@example.com>
2024-03-12 18:07:26 +0800

    fixing task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let occurences = 0;
-	for (let i = 0; i < list.length; i++) {
-		if (list[i] == searchElement) { occurences += 1 };
-	}
-	return (occurences)
-}
+  let occurences = 0;
+  if (list && searchElement) {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === searchElement) { occurences += 1; }
+    }
+  }
+  return (occurences);
+};
