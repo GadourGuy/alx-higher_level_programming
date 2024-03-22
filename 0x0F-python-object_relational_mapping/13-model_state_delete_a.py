3d1cb6154abe490856b829490cdcd833378e2d23
Your Name <you@example.com>
2024-03-22 11:43:14 +0800

    adding task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""fetching all elemnts"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).all()
+    for state in result:
+        if 'a' in state.name:
+            session.delete(state)
+            session.commit()
