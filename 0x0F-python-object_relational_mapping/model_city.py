2fe101e0e3ad7f1df225fa293f8e741fe181dcbc
Your Name <you@example.com>
2024-03-22 12:45:25 +0800

    fixing last one

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-
-"""making module_state to work with"""
+"""
+Contains the class definition of a City
+"""
 from model_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
-
-
 class City(Base):
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
