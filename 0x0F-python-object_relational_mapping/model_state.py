a18dd7ede952e1469ae3405fbf1b4e240f226a6d
Your Name <you@example.com>
2024-03-22 10:33:36 +0800

    adding task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""making module_state to work with"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+	"""state class with id attribute"""
+
+	__tablename__ = 'states'
+	id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+	name = Column(String(128), nullable=False)
