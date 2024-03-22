8f5ccd4d17612cf1185442f73dcf753f496e845a
Your Name <you@example.com>
2024-03-22 12:37:04 +0800

    adding last task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""making module_state to work with"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
