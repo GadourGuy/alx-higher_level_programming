e9d76929216ba0ca07c89d2738de0f7123e28740
Your Name <you@example.com>
2024-03-22 11:32:36 +0800

    adding task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""fetching all elemnts"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    result = session.query(State).filter_by(name='Louisiana').first()
+    print(result.id)
+    session.commit()
+    
