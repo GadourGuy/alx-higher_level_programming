97cdd2a6580fab3cd95319783ab034cbf6bbc841
GadourGuy <ogadours1@example.com>
2024-04-27 06:42:57 +0800

    adding task 0 & 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ in a specific way """
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as result:
-        content = result.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as re:
+        content = re.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
