3b82cdd14f587aa258c613a32271931610479c52
GadourGuy <ogadours1@example.com>
2024-04-27 06:59:31 +0800

    adding tasks till 6

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Displays the X-Request-Id value to a fetched URL"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
